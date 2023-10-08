e83ef2a9aefab3251905a285daec0b76ecc7d39e
Alimardon <alimardontoshpulatov001@gmail.com>
2023-10-08 13:02:23 +0500

    added visibleDB and changed seriesDB

--- script.js
+++ script.js
@@ -16,49 +16,46 @@ const seriesDB = {
             seriesDB.count = +prompt("Nechta serial ko'rdingiz?", "");
         }
     },
-};
+    rememberMySeries: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt("Oxirgi ko'rgan serialingiz?", ""),
+                b = prompt("Nechi baxo berasiz?", "");
 
-function rememberMySeries() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt("Oxirgi ko'rgan serialingiz?", ""),
-            b = prompt("Nechi baxo berasiz?", "");
-
-        if (a != null && b != null && a != "" && b != "") {
-            seriesDB.series[a] = b;
-            console.log("done");
-        } else {
-            console.log("error");
-            i--;
+            if (a != null && b != null && a != "" && b != "") {
+                seriesDB.series[a] = b;
+                console.log("done");
+            } else {
+                console.log("error");
+                i--;
+            }
         }
-    }
-}
-
-rememberMySeries();
-
-function detectLevelSeries(qiymat) {
-    if (qiymat < 5) {
-        console.log("Kam serial ko'ribsiz");
-    } else if (qiymat >= 5 && qiymat < 10) {
-        console.log("Siz classik tomoshabin ekansiz");
-    } else if (qiymat >= 10) {
-        console.log("Siz serialchi zvezda ekansiz");
-    } else {
-        console.log("Error");
-    }
-}
-
-detectLevelSeries(seriesDB.count);
-
-function showDB(hidn) {
-    if (!hidn) {
-        console.log(seriesDB);
-    }
-}
-showDB(seriesDB.private);
-
-function writeGenres() {
-    for (let i = 0; i < 3; i++) {
-        seriesDB.genres[i] = prompt(`Yaxshi ko'rgan janiringiz #${i + 1}`);
-    }
-}
-writeGenres();
+    },
+    detectLevelSeries: function () {
+        if (seriesDB.count < 5) {
+            console.log("Kam serial ko'ribsiz");
+        } else if (seriesDB.count >= 5 && seriesDB.count < 10) {
+            console.log("Siz classik tomoshabin ekansiz");
+        } else if (seriesDB.count >= 10) {
+            console.log("Siz serialchi zvezda ekansiz");
+        } else {
+            console.log("Error");
+        }
+    },
+    showDB: function () {
+        if (!seriesDB.private) {
+            console.log(seriesDB);
+        }
+    },
+    visibleDB: function () {
+        if (seriesDB.private) {
+            seriesDB.private = false;
+        } else {
+            seriesDB.private = true;
+        }
+    },
+    writeGenres: function () {
+        for (let i = 0; i < 3; i++) {
+            seriesDB.genres[i] = prompt(`Yaxshi ko'rgan janiringiz #${i + 1}`);
+        }
+    },
+};
